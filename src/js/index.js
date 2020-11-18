1ef4d5e05770aecac6c26e06d3ed3cbb9d192852
matheus2x <matheushenriqueplus99@gmail.com>
2020-11-18 20:09:54 -0300

    refactor: put nodePort in a var

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import "../css/styles.css";
 
+const nodePort = 3333;
+const baseUrl = `http://localhost:${nodePort}`;
+
 const registerBtn = document.querySelector("#register-btn");
 const form = document.querySelector("#comment-form");
 const commentInput = document.querySelector("#comment-text");
@@ -28,7 +31,7 @@ form.addEventListener("submit", (event) => {
     }
   }
   
-  fetch("http://localhost:3333/tts", {
+  fetch(`${baseUrl}/tts`, {
     method: fetchConfigs.method,
     headers: fetchConfigs.headers,
     body: jsonRequest
@@ -47,7 +50,7 @@ form.addEventListener("submit", (event) => {
     const textComment = document.createElement("p");
     textComment.innerHTML = result.comment;
     
-    const mp3 = `http://localhost:3333/uploads/${result.speech}`;
+    const mp3 = `${baseUrl}/uploads/${result.speech}`;
     
     const mp3src = document.createElement("source");
     mp3src.setAttribute("src", mp3);
