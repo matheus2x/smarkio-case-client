707ec10d84179b2225de1e218a31370d9affe743
matheus2x <matheushenriqueplus99@gmail.com>
2020-11-18 15:21:58 -0300

    fix: change css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,41 @@ const  HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-   entry: "./src/js/index.js",
-   output: {
-     filename: "main.js",
-     path: resolve(__dirname, "dist"),
-   },
-   module: {
-     rules: [{
-       test: /\.css$/,
-       use: [
-         "style-loader",
-         "css-loader",
-         MiniCssExtractPlugin.loader,
-       ],
-     }, {
-       test: /\.(png|svg|jpe?g|gif|mp3|mpe?g)$/,
-       loader: "file-loader",
-       options: {
-         name: "[name].[ext]"
-       }
-     }, {
-       test: /\.js$/,
-       exclude: /node_modules/,
-       use: {
-         loader: "babel-loader",
-         options: {
-           presets: ["@babel/preset-env"]
-         }
-        }
-     }]
-   },
-   plugins: [
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: resolve(__dirname, "dist"),
+  },
+  plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "./src/index.html"
+      template: "./src/index.html",
     }),
     new MiniCssExtractPlugin({
       filename: "styles.css",
     })
-   ]
- }
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [ MiniCssExtractPlugin.loader , "css-loader" ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif|mp3|mpe?g)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+    ],
+  },
+}
