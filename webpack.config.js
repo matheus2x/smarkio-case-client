48305911c0122859f5ef8ad5f06c03adeab512bd
matheus2x <matheushenriqueplus99@gmail.com>
2021-01-15 22:40:45 -0300

    chore: add bootstrap setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,16 @@
 const { resolve } = require("path");
 const  HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { DuplicatesPlugin } = require("inspectpack/plugin");
+
 
 module.exports = {
+  mode: "production",
   entry: [ "./src/js/index.js"],
   output: {
-    filename: "main.js",
-    path: resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+    path: resolve(__dirname, "dist")
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    })
-  ],
   module: {
     rules: [
       {
@@ -28,16 +22,47 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: [ MiniCssExtractPlugin.loader , "css-loader" ],
+        test: /\.(png|svg|jpe?g|gif|mp3|mpe?g)$/i,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "static",
+            publicPath: "static",
+            emitFile: true,
+            esModule: false,
+          }
+        }]
       },
       {
-        test: /\.(png|svg|jpe?g|gif|mp3|mpe?g)$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-        },
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader ,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ]
       },
     ],
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new DuplicatesPlugin({
+      emitErrors: false,
+      emitHandler: undefined,
+      verbose: false
+    })
+  ],
 }
